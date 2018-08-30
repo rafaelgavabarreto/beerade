563dd76acd8d0d8cac5890a844cf7410bb2755a1
rafaelgavabarreto <rafaelgavabarreto@gmail.com>
2018-08-30 01:02:04 -0400

    Page datails with opinions of users

--- routes/detail.js
+++ routes/detail.js
@@ -18,25 +18,53 @@ var lcboApi = {
 
 module.exports = (knex) => {
 
-  router.get("/detail", (req, res) => {
-    res.render("detail");
-  });
+  // router.get("/detail", (req, res) => {
+  //   res.render("detail");
+  // });
 
-  router.post("/detail/:id", (req, res) => {
+  router.get("/detail/:id", (req, res) => {
 
     lcboApi.uri = 'https://lcboapi.com/products/'+req.params.id;
   
     rp(lcboApi)
     .then(function (repos) {
-      const templateVars = {
-        username: req.session.username,
-        database: repos.result
-      };
-      console.log(repos);
-      res.render("detail",templateVars);
+
+      knex.select('*')
+        .from('opinions')
+        .where('id_beer', '=', req.params.id)
+        .then(function (results) {
+          const templateVars = {
+            username: req.session.username,
+            user_id: req.session.user_id,
+            opinions: results,
+            database: repos.result
+          };
+          console.log(templateVars);
+          res.render("detail",templateVars);
+        })
+        .catch(function (error) {
+          console.error(error)
+        });
     });
   });
 
+  router.post("/new_opinion", (req, res) => {
+
+    knex('opinions')
+      .insert([{
+        id_user: req.session.user_id,
+        id_beer: req.body.beer_id,
+        opinion: req.body.opinion,
+        rate: req.body.rate
+      }])
+      .then(function () {
+        res.redirect("/api/details/detail/" + req.body.beer_id);
+      })
+      .catch(function (error) {
+        console.error('Error: Inserting the user', error)
+      });
+
+  });
 
   return router;
 }
